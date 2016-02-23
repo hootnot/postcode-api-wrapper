d8be917d859e56d98e0783a3e8b1501c06423df3
hootnot <f.brekeveld@gmail.com>
2016-02-23 13:20:23 +0100

    PEP8 modification

--- postcodepy/postcodepy.py
+++ postcodepy/postcodepy.py
@@ -162,9 +162,9 @@ class PostcodeError(Exception):
 if __name__ == "__main__":
     import sys
     import os
-    """First and third are OK, the 2nd is not OK and raises an Exception
-       the exception is written to stderr
-    """
+    # First and third are OK, the 2nd is not OK and raises an Exception
+    # the exception is written to stderr
+
     api = API(environment='live', access_key=os.getenv("ACCESS_KEY"),
               access_secret=os.getenv("ACCESS_SECRET"))
     for pc in [('1071XX', 1), ('8422DH', 34, 'B'),
@@ -182,4 +182,5 @@ if __name__ == "__main__":
                     print("ERROR: ", K, retValue[K])
 
         except PostcodeError as e:
-            sys.stderr.write("%s, %s, %s, %s" % (e, pc, e.exceptionId, e.response_data))
+            sys.stderr.write("%s, %s, %s, %s" %
+                             (e, pc, e.exceptionId, e.response_data))
